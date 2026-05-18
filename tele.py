51f19e445e6a00e1b27093c6d8e52853000f5b30
OriErez3 <orierez3@gmail.com>
2026-05-18 15:21:36 -0400

    Added the simple functionality of adding Gemini to the telegram bot. You can now speak to gemini from telegram, which is step 1 of this project.

--- tele.py
+++ tele.py
@@ -2,25 +2,33 @@ import telegram
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters
 from dotenv import load_dotenv
 import os
+from google import genai
 
 load_dotenv()
 
 telegram_key = os.getenv("TELEGRAM_TOKEN")
+gemini_key = os.getenv("GEMINI_API_KEY")
 if telegram_key is None:
     raise ValueError("TELEGRAM_TOKEN environment variable is required")
+if gemini_key is None:
+    raise ValueError("GEMINI_API_KEY environment variable is required")
 
-
+client = genai.Client(api_key=gemini_key)
 
 async def start(update: telegram.Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Hello! I'm your Telegram bot.")
 
-async def echo(update: telegram.Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(update.message.text)
+async def respond(update: telegram.Update, context: ContextTypes.DEFAULT_TYPE):
+    user_message = update.message.text
+    response = client.models.generate_content(
+        model="gemini-3.1-flash-lite", 
+        contents=user_message)
+    await update.message.reply_text(response.text)
 
 def main() -> None:
     application = ApplicationBuilder().token(telegram_key).build()
     application.add_handler(CommandHandler("start", start))
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, respond))
     application.run_polling(allowed_updates=telegram.Update.ALL_TYPES)
 
 if __name__ == "__main__":
